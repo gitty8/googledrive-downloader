dfd3fe6a3ea545fe8a1282c8e86a94cf52dba8a5
gitty8 <github@qwer.nu>
2020-01-06 16:18:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-$(function () {
-  var button = $('<div class="multiple-download">Multiple Downloads</div>');
-  button.prependTo('[role="toolbar"]:has([data-tooltip="Preview"])');
-  button.click(function () {
-    var selecteds = $('[data-is-doc-name][data-id][aria-selected="true"]');
-    selecteds.each(function () {
-      var dataId = $(this).attr('data-id');
-      var url = `https://drive.google.com/uc?id=${dataId}&export=download`;
-      var frame = $(`<iframe src="${url}" frameborder="0" style="display: none"></iframe>`);
-      frame.appendTo(this);
-    });
-  });
+var selecteds = document.querySelectorAll('[data-id][aria-selected="true"]');
+selecteds.forEach(function (element) {
+  var dataId = element.getAttribute('data-id');
+  var frame = document.createElement("iframe");
+  frame.src = `https://drive.google.com/uc?id=${dataId}&export=download`;
+  frame.style = 'display: none';
+  element.appendChild(frame);
 });
